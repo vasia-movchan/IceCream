cdf3863286a5b956ccc3b0bce54c0904a1da92ee
Serj <mrnobody959@gmail.com>
2022-02-19 21:46:28 +0200

    fix for js functionality

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -3,13 +3,15 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     mobileMenu: document.querySelector('[data-menu]'),
+    backDrop: document.querySelector('[menu-backdrop]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    document.body.classList.toggle("menu-open");
+    refs.backDrop.classList.toggle('is-hidden');
     refs.mobileMenu.classList.toggle('is-hidden');
+    document.body.classList.toggle("menu-open");
   }
 })();
