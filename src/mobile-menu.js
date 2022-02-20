2c5f5c251c370a865c56cd092849add6c16d432f
Serj <mrnobody959@gmail.com>
2022-02-20 09:56:19 +0200

    update header functionality

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -2,8 +2,12 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
-    mobileMenu: document.querySelector('[data-menu]'),
     backDrop: document.querySelector('[menu-backdrop]'),
+    closeHomeLink: document.querySelector('[menu-home-link]'),
+    closeAboutLink: document.querySelector('[menu-about-link]'),
+    closeHowLink: document.querySelector('[menu-how-link]'),
+    closeOurLink: document.querySelector('[menu-our-link]'),
+    closeContactLink: document.querySelector('[menu-contact-link]'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -11,7 +15,17 @@
 
   function toggleMenu() {
     refs.backDrop.classList.toggle('is-hidden');
-    refs.mobileMenu.classList.toggle('is-hidden');
+    document.body.classList.toggle("menu-open");
+  }
+
+  refs.closeHomeLink.addEventListener('click', toggleLink);
+  refs.closeAboutLink.addEventListener('click', toggleLink);
+  refs.closeHowLink.addEventListener('click', toggleLink);
+  refs.closeOurLink.addEventListener('click', toggleLink);
+  refs.closeContactLink.addEventListener('click', toggleLink);
+
+  function toggleLink() {
+    refs.backDrop.classList.toggle('is-hidden');
     document.body.classList.toggle("menu-open");
   }
 })();
